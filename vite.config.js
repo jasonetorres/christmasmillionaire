e308f60cc4497defc1a2d6e58de365144cd9a988
Jason <jasontorres585@icloud.com>
2025-12-18 09:53:28 -0500

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,10 @@ export default defineConfig({
     build: {
         outDir: 'dist',
     },
+    server: {
+        historyApiFallback: true,
+    },
+    preview: {
+        historyApiFallback: true,
+    },
 });
