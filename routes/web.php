89d909717491704ef5eaedbee89fd28001c95f7e
Jason <jasontorres585@icloud.com>
2025-12-18 09:26:17 -0500

    Added index.html

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\GameController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [GameController::class, 'welcome'])->name('welcome');
-Route::get('/host', [GameController::class, 'host'])->name('host');
-Route::get('/display', [GameController::class, 'display'])->name('display');
-Route::get('/vote', [GameController::class, 'vote'])->name('vote');
